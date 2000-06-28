438d87af5a1d5cea65d6c9897baccc0f96416083
Dave Beckett <dave@dajobe.org>
2000-06-28 17:29:17 +0000

    RDF Iterator definition

---
+++ librdf/rdf_iterator.h
@@ -0,0 +1,23 @@
+/*
+ * RDF Iterator definition
+ *
+ * $Source$
+ * $Id$
+ *
+ * (C) Dave Beckett 2000 ILRT, University of Bristol
+ * http://www.ilrt.bristol.ac.uk/people/cmdjb/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ */
+
+
+#ifndef RDF_ITERATOR_H
+#define RDF_ITERATOR_H
+
+typedef char rdf_iterator;
+
+#endif
