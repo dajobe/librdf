1ea21d6ed331fb46bd0cc302b5ce32daedba39cb
Dave Beckett <dave@dajobe.org>
2000-06-29 09:44:42 +0000

    Added more SSL crypto defs

--- acconfig.h
+++ acconfig.h
@@ -17,9 +17,10 @@
 #undef HAVE_RIPEM160_DIGEST
 
 /* defined if specific message digests are available */
+#undef HAVE_OPENSSL_DIGESTS
 #undef HAVE_OPENSSL_CRYPTO_MD5_DIGEST
 #undef HAVE_OPENSSL_CRYPTO_SHA1_DIGEST
-#undef HAVE_OPENSSL_CRYPTO_RIPEM160_DIGEST
+#undef HAVE_OPENSSL_CRYPTO_RIPEMD160_DIGEST
 
 /* defined if specific hashes are available */
 #undef HAVE_GDBM_HASH
