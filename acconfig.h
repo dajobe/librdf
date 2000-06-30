1ee54fc5807f1a651d7a56bfac7a4e2ae19f03a2
Dave Beckett <dave@dajobe.org>
2000-06-30 13:53:01 +0000

    Tidy openssl digest defs

--- acconfig.h
+++ acconfig.h
@@ -11,13 +11,14 @@
 #undef HAVE_U16_TYPEDEF
 #undef HAVE_U32_TYPEDEF
 
-/* defined if specific message digests are available */
-#undef HAVE_SHA1_DIGEST
-#undef HAVE_MD5_DIGEST
-#undef HAVE_RIPEM160_DIGEST
+/* defined if have local version of message digests */
+#undef HAVE_LOCAL_SHA1_DIGEST
+#undef HAVE_LOCAL_MD5_DIGEST
+#undef HAVE_LOCAL_RIPEMD160_DIGEST
 
-/* defined if specific message digests are available */
+/* defined if have openssl digests library (libcrypto) available */
 #undef HAVE_OPENSSL_DIGESTS
+/* defined if have openssl version of message digests */
 #undef HAVE_OPENSSL_CRYPTO_MD5_DIGEST
 #undef HAVE_OPENSSL_CRYPTO_SHA1_DIGEST
 #undef HAVE_OPENSSL_CRYPTO_RIPEMD160_DIGEST
