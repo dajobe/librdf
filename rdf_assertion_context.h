96a22359f7b5038ab0a3cc51ef4c4b6dc343e560
Dave Beckett <dave@dajobe.org>
2000-06-30 16:32:04 +0000

    RDF Assertion Context definition

---
+++ rdf_assertion_context.h
@@ -0,0 +1,23 @@
+/*
+ * RDF Assertion Context definition
+ *
+ * $Source$
+ * $Id$
+ *
+ * (C) Dave Beckett 2000 ILRT, University of Bristol
+ * http://www.ilrt.bristol.ac.uk/people/cmdjb/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ */
+
+
+#ifndef RDF_ASSERTION_CONTEXT_H
+#define RDF_ASSERTION_CONTEXT_H
+
+typedef char  rdf_assertion_context;
+
+#endif
