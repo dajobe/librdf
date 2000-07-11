c5055174446f0ea917813c7b6de5f6b06ac218c6
Dave Beckett <dave@dajobe.org>
2000-07-11 17:49:56 +0000

    Comment all public functions, indented.

--- librdf/rdf_digest_openssl.c
+++ librdf/rdf_digest_openssl.c
@@ -28,76 +28,80 @@
 #ifdef HAVE_OPENSSL_CRYPTO_MD5_DIGEST
 #include <openssl/md5.h>
 
-/* New structure contains old at start (so casting works) plus
-   an area for the digest when calculated */
+
+/* The new struct contains the old one at start (so casting works) plus
+ * a space for the digest to be stored once calculated
+ */
 typedef struct 
 {
-  MD5_CTX contex;
-  unsigned char digest[MD5_DIGEST_LENGTH];
+        MD5_CTX contex;
+        unsigned char digest[MD5_DIGEST_LENGTH];
 } MD5_CTX_2;
 
 
 static void
 md5_final(MD5_CTX_2 *c) 
 {
-  MD5_Final(c->digest, (MD5_CTX*)c);
+        MD5_Final(c->digest, (MD5_CTX*)c);
 }
 
 static unsigned char *
 md5_get_digest(MD5_CTX_2 *c)
 {
-  return c->digest;
+        return c->digest;
 }
 
 
 static void
 librdf_openssl_md5_register_factory(librdf_digest_factory *factory) 
 {
-  factory->context_length = sizeof(MD5_CTX_2);
-  factory->digest_length = MD5_DIGEST_LENGTH;
-
-  factory->init  = (void (*)(void *))MD5_Init;
-  factory->update = (void (*)(void *, unsigned char*, size_t))MD5_Update;
-  factory->final = (void (*)(void *))md5_final;
-  factory->get_digest  = (unsigned char *(*)(void *))md5_get_digest;
+        factory->context_length = sizeof(MD5_CTX_2);
+        factory->digest_length = MD5_DIGEST_LENGTH;
+        
+        factory->init  = (void (*)(void *))MD5_Init;
+        factory->update = (void (*)(void *, unsigned char*, size_t))MD5_Update;
+        factory->final = (void (*)(void *))md5_final;
+        factory->get_digest  = (unsigned char *(*)(void *))md5_get_digest;
 }
 #endif
+
 
 #ifdef HAVE_OPENSSL_CRYPTO_SHA1_DIGEST
 #include <openssl/sha.h>
 
-/* New structure contains old at start (so casting works) plus
-   an area for the digest when calculated */
+/* The new struct contains the old one at start (so casting works) plus
+ * a space for the digest to be stored once calculated
+ */
 typedef struct 
 {
-  SHA_CTX contex;
-  unsigned char digest[SHA_DIGEST_LENGTH];
+        SHA_CTX contex;
+        unsigned char digest[SHA_DIGEST_LENGTH];
 } SHA_CTX_2;
 
 
 static void
 sha1_final(SHA_CTX_2 *c) 
 {
-  SHA1_Final(c->digest, (SHA_CTX*)c);
+        SHA1_Final(c->digest, (SHA_CTX*)c);
 }
 
 static unsigned char *
 sha1_get_digest(SHA_CTX_2 *c)
 {
-  return c->digest;
+        return c->digest;
 }
 
 
 static void
 librdf_openssl_sha1_register_factory(librdf_digest_factory *factory) 
 {
-  factory->context_length = sizeof(SHA_CTX_2);
-  factory->digest_length = SHA_DIGEST_LENGTH;
-
-  factory->init  = (void (*)(void *))SHA1_Init;
-  factory->update = (void (*)(void *, unsigned char*, size_t))SHA1_Update;
-  factory->final = (void (*)(void *))sha1_final;
-  factory->get_digest  = (unsigned char *(*)(void *))sha1_get_digest;
+        factory->context_length = sizeof(SHA_CTX_2);
+        factory->digest_length = SHA_DIGEST_LENGTH;
+        
+        factory->init  = (void (*)(void *))SHA1_Init;
+        factory->update = (void (*)(void *, unsigned char*, size_t))SHA1_Update;
+        factory->final = (void (*)(void *))sha1_final;
+        factory->get_digest  = (unsigned char *(*)(void *))sha1_get_digest;
 }
 #endif
 
@@ -105,53 +109,63 @@ librdf_openssl_sha1_register_factory(librdf_digest_factory *factory)
 #ifdef HAVE_OPENSSL_CRYPTO_RIPEMD160_DIGEST
 #include <openssl/ripemd.h>
 
-/* New structure contains old at start (so casting works) plus
-   an area for the digest when calculated */
+/* The new struct contains the old one at start (so casting works) plus
+ * a space for the digest to be stored once calculated
+ */
 typedef struct 
 {
-  RIPEMD160_CTX contex;
-  unsigned char digest[RIPEMD160_DIGEST_LENGTH];
+        RIPEMD160_CTX contex;
+        unsigned char digest[RIPEMD160_DIGEST_LENGTH];
 } RIPEMD160_CTX_2;
 
 
 static void
 ripemd160_final(RIPEMD160_CTX_2 *c) 
 {
-  RIPEMD160_Final(c->digest, (RIPEMD160_CTX*)c);
+        RIPEMD160_Final(c->digest, (RIPEMD160_CTX*)c);
 }
 
 static unsigned char *
 ripemd160_get_digest(RIPEMD160_CTX_2 *c)
 {
-  return c->digest;
+        return c->digest;
 }
 
 
 static void
 librdf_openssl_ripemd160_register_factory(librdf_digest_factory *factory) 
 {
-  factory->context_length = sizeof(RIPEMD160_CTX_2);
-  factory->digest_length = SHA_DIGEST_LENGTH;
+        factory->context_length = sizeof(RIPEMD160_CTX_2);
+        factory->digest_length = SHA_DIGEST_LENGTH;
 
-  factory->init  = (void (*)(void *))RIPEMD160_Init;
-  factory->update = (void (*)(void *, unsigned char*, size_t))RIPEMD160_Update;
-  factory->final = (void (*)(void *))ripemd160_final;
-  factory->get_digest  = (unsigned char *(*)(void *))ripemd160_get_digest;
+        factory->init  = (void (*)(void *))RIPEMD160_Init;
+        factory->update = (void (*)(void *, unsigned char*, size_t))RIPEMD160_Update;
+        factory->final = (void (*)(void *))ripemd160_final;
+        factory->get_digest  = (unsigned char *(*)(void *))ripemd160_get_digest;
 }
 #endif
 
 
+/**
+ * librdf_digest_openssl_constructor:
+ * @void: 
+ * 
+ * Initialise the OpenSSL digest module
+ **/
 void
 librdf_digest_openssl_constructor(void)
 {
 #ifdef HAVE_OPENSSL_CRYPTO_MD5_DIGEST
-  librdf_digest_register_factory("MD5", &librdf_openssl_md5_register_factory);
+        librdf_digest_register_factory("MD5",
+                                       &librdf_openssl_md5_register_factory);
 #endif
 #ifdef HAVE_OPENSSL_CRYPTO_RIPEMD160_DIGEST
-  librdf_digest_register_factory("RIPEMD160", &librdf_openssl_ripemd160_register_factory);
+        librdf_digest_register_factory("RIPEMD160",
+                                       &librdf_openssl_ripemd160_register_factory);
 #endif
 #ifdef HAVE_OPENSSL_CRYPTO_SHA1_DIGEST
-  librdf_digest_register_factory("SHA1", &librdf_openssl_sha1_register_factory);
+        librdf_digest_register_factory("SHA1",
+                                       &librdf_openssl_sha1_register_factory);
 #endif
 
 }
