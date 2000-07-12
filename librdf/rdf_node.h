f236b55c524cff3ca4177b2e9b1a5fbb18c2a78c
Dave Beckett <dave@dajobe.org>
2000-07-12 14:19:37 +0000

    Indented. Made node type be enum librdf_node_type instead of int.

--- librdf/rdf_node.h
+++ librdf/rdf_node.h
@@ -28,32 +28,34 @@
 extern "C" {
 #endif
 
-/* A resource (also a property) - has a URI */
-#define LIBRDF_NODE_TYPE_RESOURCE 0
-/* A literal - an XML string + language */
-#define LIBRDF_NODE_TYPE_LITERAL  1
+/* Node types */
+typedef enum {
+	LIBRDF_NODE_TYPE_RESOURCE, /* A resource (& property) - has a URI */
+	LIBRDF_NODE_TYPE_LITERAL   /* A literal - an XML string + language */
+} librdf_node_type;
+
 
 typedef struct 
 {
-  int type;
-  union
-  {
-    union
-    {
-      /* resources have URIs */
-      librdf_uri *uri;
-    } resource;
-    struct
-    {
-      /* literals have string values and maybe an XML language */
-      char *string;
-      int string_len;
-      /* XML Language */
-      char *xml_language;
-      /* Is the literal "Well Formed XML?" */
-      int is_wf_xml;
-    } literal;
-  } value;
+	librdf_node_type type;
+	union
+	{
+		union
+		{
+			/* resources have URIs */
+			librdf_uri *uri;
+		} resource;
+		struct
+		{
+			/* literals have string values ... */
+			char *string;
+			int string_len;
+			/* .. and optional XML Language */
+			char *xml_language;
+			/* and may be "Well Formed XML" */
+			int is_wf_xml;
+		} literal;
+	} value;
 }
 librdf_node;
 
@@ -89,8 +91,8 @@ void librdf_free_node(librdf_node *r);
 librdf_uri* librdf_node_get_uri(librdf_node* node);
 int librdf_node_set_uri(librdf_node* node, librdf_uri *uri);
 
-int librdf_node_get_type(librdf_node* node);
-void librdf_node_set_type(librdf_node* node, int type);
+librdf_node_type librdf_node_get_type(librdf_node* node);
+void librdf_node_set_type(librdf_node* node, librdf_node_type type);
 
 char* librdf_node_get_literal_value(librdf_node* node);
 char* librdf_node_get_literal_value_language(librdf_node* node);
