5a7282a9c80744d2928ccc934070e5251a5c0ff9
Dave Beckett <dave@dajobe.org>
2000-07-17 20:19:08 +0000

    Indenting, addeddd finished function / element. (librdf_iterator_have_elements): Added (librdf_iterator_get_next): Added

--- librdf/rdf_iterator.c
+++ librdf/rdf_iterator.c
@@ -26,26 +26,67 @@
 #include <rdf_iterator.h>
 
 
+/**
+ * librdf_new_iterator:
+ * @context: context to pass to the iterator functions
+ * @have_elements: function to call to see if there are more elements
+ * @get_next: function to get the next element
+ * @finished: function to destroy the iterator context (or NULL if not needed)
+ * 
+ * Constructor: create a new &librdf_iterator object.
+ * 
+ * Return value: a new &librdf_iterator object or NULL on failure
+**/
 librdf_iterator*
-librdf_new_iterator(void* datum,
-                 int (*have_elements)(void*),
-                 void* (*get_next)(void*))
+librdf_new_iterator(void* context,
+		    int (*have_elements)(void*),
+		    void* (*get_next)(void*),
+		    void (*finished)(void*))
 {
-  librdf_iterator* new_iterator;
-  
-  new_iterator=(librdf_iterator*)LIBRDF_CALLOC(librdf_iterator, 1, sizeof(librdf_iterator));
-  if(!new_iterator)
-    return NULL;
+	librdf_iterator* new_iterator;
+	
+	new_iterator=(librdf_iterator*)LIBRDF_CALLOC(librdf_iterator, 1, 
+						     sizeof(librdf_iterator));
+	if(!new_iterator)
+		return NULL;
 
-  new_iterator->have_elements=have_elements;
-  new_iterator->get_next=get_next;
+	new_iterator->context=context;
+
+	new_iterator->have_elements=have_elements;
+	new_iterator->get_next=get_next;
+	new_iterator->finished=finished;
   
-  return new_iterator;
+	return new_iterator;
 }
 
+
+/**
+ * librdf_free_iterator:
+ * @iterator: 
+ * 
+ * 
+ **/
 void
 librdf_free_iterator(librdf_iterator* iterator) 
 {
-  LIBRDF_FREE(librdf_iterator, iterator);
+	if(iterator->finished)
+		iterator->finished(iterator->context);
+	LIBRDF_FREE(librdf_iterator, iterator);
+}
+
+
+
+
+int
+librdf_iterator_have_elements(librdf_iterator* iterator) 
+{
+	return iterator->have_elements(iterator->context);
+}
+
+
+void*
+librdf_iterator_get_next(librdf_iterator* iterator) 
+{
+	return iterator->get_next(iterator->context);
 }
 
