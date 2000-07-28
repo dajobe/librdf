b0ee854f82557af1bba92e962097e33975cbd17f
Dave Beckett <dave@dajobe.org>
2000-07-28 10:13:56 +0000

    Comment header change Use new include files Indented again (librdf_init_world): Re-ordered to create digest factory first, since it is used by other modules.

--- librdf/rdf_init.c
+++ librdf/rdf_init.c
@@ -1,10 +1,7 @@
-/*
- * rdf_init.c - RDF Node implementation
- *   RDF:Resource
- *   RDF:Property
- *   (object) - RDF:Literal / RDF:Resource
+/* -*- Mode: c; c-basic-offset: 2 -*-
  *
- * $Source$
+ * rdf_init.c - Overall librdf initialisation / termination
+ *
  * $Id$
  *
  * (C) Dave Beckett 2000 ILRT, University of Bristol
@@ -20,12 +17,12 @@
  */
 
 
-#include <config.h>
+#include <rdf_config.h>
 
 #include <stdio.h>
 
 #define LIBRDF_INTERNAL 1
-#include <rdf_config.h>
+#include <librdf.h>
 #include <rdf_hash.h>
 #include <rdf_uri.h>
 #include <rdf_node.h>
@@ -39,27 +36,30 @@
 void
 librdf_init_world(char *digest_factory_name) 
 {
-	librdf_digest_factory* digest_factory;
-	
-	digest_factory=librdf_get_digest_factory(digest_factory_name);
-	
-	librdf_init_digest();
-	librdf_init_hash();
-	librdf_init_uri(digest_factory);
-	librdf_init_node(digest_factory);
-	librdf_init_statement();
-	librdf_init_model();
-	librdf_init_storage();
+  librdf_digest_factory* digest_factory;
+  
+  librdf_init_digest();
+
+  digest_factory=librdf_get_digest_factory(digest_factory_name);
+ 
+  librdf_init_uri(digest_factory);
+  librdf_init_node(digest_factory);
+
+  librdf_init_hash();
+  librdf_init_statement();
+  librdf_init_model();
+  librdf_init_storage();
 }
 
 
 void
 librdf_destroy_world(void)
 {
-	librdf_finish_digest();
-	librdf_finish_hash();
-	/* librdf_finish_uri(); */
-	/* librdf_finish_node(); */
-	librdf_finish_statement();
-	/* librdf_finish_model(); */
+  librdf_finish_digest();
+  librdf_finish_hash();
+  /* librdf_finish_uri(); */
+  /* librdf_finish_node(); */
+  librdf_finish_statement();
+  librdf_finish_model();
+  librdf_finish_storage();
 }
