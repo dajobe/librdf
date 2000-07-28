f489072330bdb4b209e39f28242b109459fa4fb8
Dave Beckett <dave@dajobe.org>
2000-07-28 10:05:52 +0000

    Comment header change Use new include files Indented again First version working with storage (librdf_free_model, librdf_model_add_submodel, librdf_model_remove_submodel): Sub models code (not tested). (librdf_model_add_statement): Documented (librdf_model_add, librdf_model_add_string_literal_statement): Implemented. (librdf_model_print): Added

--- librdf/rdf_model.c
+++ librdf/rdf_model.c
@@ -1,7 +1,7 @@
-/*
+/* -*- Mode: c; c-basic-offset: 2 -*-
+ *
  * rdf_model.c - RDF Model implementation
  *
- * $Source$
  * $Id$
  *
  * (C) Dave Beckett 2000 ILRT, University of Bristol
@@ -17,7 +17,7 @@
  */
 
 
-#include <config.h>
+#include <rdf_config.h>
 
 #include <stdio.h>
 
@@ -26,24 +26,49 @@
 #endif
 
 #define LIBRDF_INTERNAL 1
-#include <rdf_config.h>
+#include <librdf.h>
+#include <rdf_list.h>
+#include <rdf_node.h>
+#include <rdf_statement.h>
 #include <rdf_model.h>
+#include <rdf_storage.h>
+#include <rdf_stream.h>
 
 
 /* class methods */
 void
 librdf_init_model(void)
 {
+  /* nothing to do here */
+}
+
+void
+librdf_finish_model(void)
+{
+  /* nothing to do here */
 }
 
 
 /* constructors */
 
-/* Create a new Model */
+/* Create a new Model with storage */
 librdf_model*
-librdf_new_model(void)
+librdf_new_model(librdf_storage *storage, librdf_hash* options)
 {
-  return NULL;
+  librdf_model* model;
+  
+  model=(librdf_model*)LIBRDF_CALLOC(librdf_model, 1, sizeof(librdf_model));
+  if(!model)
+    return 0;
+  
+  if(storage && librdf_storage_open(storage, model)) {
+    LIBRDF_FREE(librdf_model, model);
+    return NULL;
+  }
+  
+  model->storage=storage;
+  
+  return model;
 }
 
 
@@ -51,15 +76,44 @@ librdf_new_model(void)
 librdf_model*
 librdf_new_model_from_model(librdf_model* model)
 {
+  abort();
   return NULL;
 }
+
+
+/* Create a new Model from a stream */
+librdf_model*
+librdf_new_model_from_stream(librdf_stream* stream)
+{
+  abort();
+  return NULL;
+}
+
 
 /* destructor */
 void
 librdf_free_model(librdf_model *model)
 {
-}
+  librdf_iterator* iterator;
+  librdf_model* m;
 
+  if(model->sub_models) {
+    iterator=librdf_list_get_iterator(model->sub_models);
+    if(iterator) {
+      while(librdf_iterator_have_elements(iterator)) {
+        m=(librdf_model*)librdf_iterator_get_next(iterator);
+        if(m)
+          librdf_free_model(m);
+      }
+      librdf_free_iterator(iterator);
+    }
+    librdf_free_list(model->sub_models);
+  } else {
+    if(model->storage)
+      librdf_storage_close(model->storage);
+  }
+  LIBRDF_FREE(librdf_model, model);
+}
 
 
 
@@ -67,100 +121,178 @@ librdf_free_model(librdf_model *model)
 int
 librdf_model_size(librdf_model* model)
 {
-  return 0;
+  return librdf_storage_size(model->storage);
 }
 
 
+/* add statements */
 
-int
-librdf_model_add(librdf_model* model, librdf_node* subject, librdf_node* property, librdf_node* object)
-{
-  return 0;
-}
-
-
-int
-librdf_model_add_string_literal_statement(librdf_model* model, librdf_node* subject, librdf_node* property, char* string)
-{
-  return 0;
-}
-
-
+/**
+ * librdf_model_add_statement:
+ * @model: model
+ * @statement: statement
+ * 
+ * Add a statement to the model - after this, the statement becomes owned by
+ * the model
+ * 
+ * Return value: non 0 on failure
+ **/
 int
 librdf_model_add_statement(librdf_model* model, librdf_statement* statement)
 {
+  return librdf_storage_add_statement(model->storage, statement);
+}
+
+
+int
+librdf_model_add_statements(librdf_model* model, librdf_stream* statement_stream)
+{
+  return librdf_storage_add_statements(model->storage, statement_stream);
+}
+
+
+int
+librdf_model_add(librdf_model* model, librdf_node* subject, 
+		 librdf_node* predicate, librdf_node* object)
+{
+  librdf_statement *statement;
+  int result;
+  
+  statement=librdf_new_statement();
+  if(!statement)
+    return 1;
+
+  librdf_statement_set_subject(statement, subject);
+  librdf_statement_set_predicate(statement, predicate);
+  librdf_statement_set_object(statement, object);
+
+  result=librdf_model_add_statement(model, statement);
+  if(result)
+    librdf_free_statement(statement);
+  
   return 0;
 }
 
 
+int
+librdf_model_add_string_literal_statement(librdf_model* model, 
+					  librdf_node* subject, 
+					  librdf_node* predicate, char* string,
+					  char *xml_language, int is_wf_xml)
+{
+  librdf_node* object;
+  int result;
+  
+  object=librdf_new_node_from_literal(string, xml_language, is_wf_xml);
+  if(!object)
+    return 1;
+  
+  result=librdf_model_add(model, subject, predicate, object);
+  if(result)
+    librdf_free_node(object);
+  
+  return result;
+}
+
+
+/* remove statements */
 int
 librdf_model_remove_statement(librdf_model* model, librdf_statement* statement)
 {
-  return 0;
+  return librdf_storage_remove_statement(model->storage, statement);
 }
 
 
+/* containment */
 int
 librdf_model_contains_statement(librdf_model* model, librdf_statement* statement)
 {
-  return 0;
+  return librdf_storage_contains_statement(model->storage, statement);
 }
 
 
-/* returns an interator that returns all statements */
-librdf_iterator*
-librdf_model_get_all_statements(librdf_model* model)
+/* serialise the entire model */
+librdf_stream*
+librdf_model_serialise(librdf_model* model)
 {
-  return NULL;
+  return librdf_storage_serialise(model->storage);
+}
+
+
+/* queries */
+librdf_stream*
+librdf_model_find_statements(librdf_model* model, 
+                             librdf_statement* statement)
+{
+  return librdf_storage_find_statements(model->storage, statement);
 }
 
 
 
-/* any of subject, property or object can be NULL */
-/* returns count of number of matching statements */
+
+/* submodels */
 int
-librdf_model_find(librdf_model* model, librdf_node* subject, librdf_node* property, librdf_node* object)
+librdf_model_add_submodel(librdf_model* model, librdf_model* sub_model)
 {
-  return 0;
-}
-
-
-/* returns iterator object that returns matching librdf_statements */
-librdf_iterator*
-librdf_model_find_(librdf_model* model, librdf_node* subject, librdf_node* property, librdf_node* object)
-{
-  return NULL;
-}
-
-
-
-/* get/set the model source URI */
-librdf_uri*
-librdf_model_get_source_uri(librdf_model* model)
-{
-  return NULL;
-}
-
-
-int
-librdf_model_set_source_uri(librdf_model* model, librdf_uri *uri)
-{
-  return 0;
-}
-
-
-int
-librdf_model_add_submodel(librdf_model* model, librdf_model* sub_model) 
-{
-  return 0;
-}
-
+  librdf_list *l=model->sub_models;
   
-int
-librdf_model_remove_submodel(librdf_model* model, librdf_model* sub_model) 
-{
+  if(!l) {
+    l=librdf_new_list();
+    if(!l)
+      return 1;
+    model->sub_models=l;
+  }
+  
+  if(!librdf_list_add(l, sub_model))
+    return 1;
+  
   return 0;
 }
+
+
+
+int
+librdf_model_remove_submodel(librdf_model* model, librdf_model* sub_model)
+{
+  librdf_list *l=model->sub_models;
+  
+  if(!l)
+    return 1;
+  if(!librdf_list_remove(l, sub_model))
+    return 1;
+  
+  return 0;
+}
+
+
+void
+librdf_model_print(librdf_model *model, FILE *fh)
+{
+  librdf_stream* stream;
+  librdf_statement* statement;
+  char *s;
+  
+  stream=librdf_model_serialise(model);
+  if(!stream)
+    return;
+  
+  while(!librdf_stream_end(stream)) {
+    statement=librdf_stream_next(stream);
+    if(!statement)
+      break;
+    s=librdf_statement_to_string(statement);
+    if(s) {
+      fputs("  ", fh);
+      fputs(s, fh);
+      fputs("\n", fh);
+      LIBRDF_FREE(cstring, s);
+    }    
+  }
+  fputs("]]\n", fh);
+  
+  librdf_free_stream(stream);
+}
+
 
 
 
@@ -174,18 +306,52 @@ int main(int argc, char *argv[]);
 int
 main(int argc, char *argv[]) 
 {
+  librdf_storage_factory* factory;
+  librdf_storage* storage;
   librdf_model* model;
+  librdf_statement *statement;
   char *program=argv[0];
   
-  /* initialise model module */
+  /* initialise statement and model modules */
+  librdf_init_statement();
+  librdf_init_storage();
   librdf_init_model();
+  
+  fprintf(stderr, "%s: Getting factory\n", program);
+  factory=librdf_get_storage_factory(NULL);
 
+  fprintf(stderr, "%s: Creating storage\n", program);
+  storage=librdf_new_storage(factory, NULL);
+  if(!storage) {
+    fprintf(stderr, "%s: Failed to create new storage\n", program);
+    return(1);
+  }
   fprintf(stderr, "%s: Creating model\n", program);
-  model=librdf_new_model();
+  model=librdf_new_model(storage, NULL);
 
+  statement=librdf_new_statement();
+  librdf_statement_set_subject(statement, librdf_new_node_from_uri_string("http://www.ilrt.bris.ac.uk/people/cmdjb/"));
+  librdf_statement_set_predicate(statement, librdf_new_node_from_uri_string("http://purl.org/dc/elements/1.1/#Creator"));
+  librdf_statement_set_object(statement, librdf_new_node_from_literal("Dave Beckett", NULL, 0));
+
+  /* after this, statement becomes owned by model */
+  librdf_model_add_statement(model, statement);
+
+  fprintf(stderr, "%s: Printing model\n", program);
+  librdf_model_print(model, stderr);
+  
   fprintf(stderr, "%s: Freeing model\n", program);
   librdf_free_model(model);
+
+  fprintf(stderr, "%s: Freeing storage\n", program);
+  librdf_free_storage(storage);
+
+  librdf_finish_model();
+  librdf_finish_storage();
+  librdf_finish_statement();
   
+  librdf_memory_report(stderr);
+	
   /* keep gcc -Wall happy */
   return(0);
 }
