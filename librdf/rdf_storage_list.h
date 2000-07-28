e86c3f9fe458626421bf2c7076588361ae3de71b
Dave Beckett <dave@dajobe.org>
2000-07-28 10:58:52 +0000

    RDF Storage List Interface definition

---
+++ librdf/rdf_storage_list.h
@@ -0,0 +1,36 @@
+/* -*- Mode: c; c-basic-offset: 2 -*-
+ *
+ * rdf_storage_list.h - RDF Storage List Interface definition
+ *
+ * $Id$
+ *
+ * (C) Dave Beckett 2000 ILRT, University of Bristol
+ * http://www.ilrt.bristol.ac.uk/people/cmdjb/
+ *
+ *                                       
+ * This program is free software distributed under either of these licenses:
+ *   1. The GNU Lesser General Public License (LGPL)
+ * OR ALTERNATIVELY
+ *   2. The modified BSD license
+ *
+ * See LICENSE.html or LICENSE.txt for the full license terms.
+ */
+
+
+
+#ifndef LIBRDF_STORAGE_LIST_H
+#define LIBRDF_STORAGE_LIST_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+void librdf_init_storage_list(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
