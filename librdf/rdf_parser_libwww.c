be3eb8d3ada2347062730c0268276f8a04351bd7
Dave Beckett <dave@dajobe.org>
2000-08-02 15:44:55 +0000

    First working version.

--- librdf/rdf_parser_libwww.c
+++ librdf/rdf_parser_libwww.c
@@ -22,10 +22,6 @@
 #include <stdio.h>
 #include <string.h>
 
-#ifdef STANDALONE
-#define LIBRDF_DEBUG 1
-#endif
-
 #define LIBRDF_INTERNAL 1
 #include <librdf.h>
 
@@ -33,6 +29,7 @@
 #include <rdf_node.h>
 #include <rdf_stream.h>
 #include <rdf_statement.h>
+#include <rdf_list.h>
 
 /* libwww includes */
 #undef PACKAGE
@@ -59,6 +56,8 @@ typedef struct {
   HTRDF* parser;            /* libwww RDF parser */
   HTRequest* request;       /* request for URI */
   librdf_statement* next;   /* next statement */
+  librdf_list *statements;  /* list containing all statements */
+  int request_done;         /* non 0 if request done */
   int end_of_stream;        /* non 0 if stream finished */
 } librdf_parser_libwww_stream_context;
 
@@ -76,25 +75,63 @@ librdf_parser_libwwW_new_triple_handler (HTRDF *rdfp, HTTriple *t,
                                          void *context) 
 {
   librdf_parser_libwww_stream_context* scontext=(librdf_parser_libwww_stream_context*)context;
+  librdf_statement* statement;
+  int object_is_literal;
+  char *s;
 
-  scontext->next=librdf_new_statement();
-  if(!scontext->next)
+  statement=librdf_new_statement();
+  if(!statement)
     return;
   
-  librdf_statement_set_subject(scontext->next, 
+  librdf_statement_set_subject(statement, 
                                librdf_new_node_from_uri_string(HTTriple_subject(t)));
   
-  librdf_statement_set_predicate(scontext->next,
+  librdf_statement_set_predicate(statement,
                                  librdf_new_node_from_uri_string(HTTriple_predicate(t)));
+
+
+  /* FIXME: I have no idea if the object points to a literal or a resource */
   
-  /* FIXME: I have no idea if literal or resource */
-  if(0) { /* if(object_is_literal) {*/
-    librdf_statement_set_object(scontext->next,
+  object_is_literal=1; /* assume the worst */
+
+  /* Try to guess if resource / literal from string by assuming it 
+   * is a URL if it matches ^[isalnum()]+:[^isblank()]+$
+   * This will be fooled by literals of form 'thing:non-blank-thing'
+   */
+  for(s=HTTriple_object(t); *s; s++) {
+    /* Find first non alphanumeric */
+    if(!isalnum(*s)) {
+      /* Better be a ':' */
+      if(*s == ':') {
+	/* check rest of string has no spaces */
+	for(;*s; s++)
+	  if(isblank(*s))
+	    break;
+	/* reached end, thus probably not a literal */
+	if(!*s)
+	  object_is_literal=0;
+	break;
+      }
+    }
+  }
+
+  if(object_is_literal) {
+    librdf_statement_set_object(statement,
                                 librdf_new_node_from_literal(HTTriple_object(t), NULL, 0));
   } else {
-    librdf_statement_set_object(scontext->next, 
+    librdf_statement_set_object(statement, 
                                 librdf_new_node_from_uri_string(HTTriple_object(t)));
   }
+
+#ifdef LIBRDF_DEBUG
+#if LIBRDF_DEBUG > 1
+  s=librdf_statement_to_string(statement);
+  fprintf(stderr, "Got new statement: %s\n", s);
+  LIBRDF_FREE(cstring, s);
+#endif
+#endif
+
+  librdf_list_add(scontext->statements, statement);
 }
 
 
@@ -120,12 +157,28 @@ librdf_parser_libwww_new_handler (HTStream *		me,
 }
 
 
+static int
+librdf_parser_libwww_terminate_handler (HTRequest * request,
+					HTResponse * response,
+					void * param, int status)
+{
+  librdf_parser_libwww_stream_context* scontext=(librdf_parser_libwww_stream_context*)param;
+
+  /* deleted by libwww */
+  scontext->parser=NULL;
+
+  /* finish event loop */
+  HTEventList_stopLoop();
+}
+
 
 
 /* This is a copy of client_profile from HT_Profile.c with one
-   crucial change so that the default type conversions are not
-   registered, and later on we can register something that assumes
-   the content is RDF -- Dave Beckett <Dave.Beckett@bristol.ac.uk>
+ * change so that the default type conversions are not
+ * registered, and later on we can register something that assumes
+ * the content is RDF.
+ *
+ * Probably don't need anywhere near all of this initialisation
 */
    
 static void
@@ -206,6 +259,16 @@ librdf_parser_libwww_client_profile (const char * AppName,
 }
 
 
+#ifdef LIBRDF_DEBUG
+#if LIBRDF_DEBUG > 1
+static int
+tracer (const char * fmt, va_list pArgs)
+{
+  return (vfprintf(stderr, fmt, pArgs));
+}
+#endif
+#endif
+
 
 static librdf_stream*
 librdf_parser_libwww_parse_from_uri(void *context, librdf_uri *uri) {
@@ -226,29 +289,47 @@ librdf_parser_libwww_parse_from_uri(void *context, librdf_uri *uri) {
 
   HTEventInit();
 
+#ifdef LIBRDF_DEBUG
+#if LIBRDF_DEBUG > 1
+  HTTrace_setCallback(tracer);
+  HTSetTraceMessageMask("*");
+#endif
+#endif
+
   /* Set up our RDF To Triple converter stream */
   HTFormat_addConversion("*/*", "www/present", HTRDFParser_new, 1.0, 0.0, 0.0);
   
   /* Set our new RDF parser instance callback */
   HTRDF_registerNewParserCallback (librdf_parser_libwww_new_handler, scontext);
 
+  /* Add a terminate handler */
+  HTNet_addAfter(librdf_parser_libwww_terminate_handler, NULL, scontext,
+		 HT_ALL, HT_FILTER_LAST);
+
+  /* Set the timeout for long we are going to wait for a response */
+  HTHost_setEventTimeout(10000);
+
   cwd= HTGetCurrentDirectoryURL();
 
   scontext->uri = HTParse(uri, cwd, PARSE_ALL);
+  HT_FREE(cwd);
   scontext->request = HTRequest_new();
 
   anchor = HTAnchor_findAddress(uri);
+  if(!anchor) {
+    librdf_parser_libwww_serialise_finished((void*)scontext);
+    return NULL;
+  }
   
   /* Start the GET request */
   status = HTLoadAnchor(anchor, scontext->request);
-  
-  /* Go into the event loop... */
+
   if (status == NO) {
     librdf_parser_libwww_serialise_finished((void*)scontext);
     return NULL;
   }
 
-
+  
   stream=librdf_new_stream((void*)scontext,
                            &librdf_parser_libwww_serialise_end_of_stream,
                            &librdf_parser_libwww_serialise_next_statement,
@@ -264,7 +345,21 @@ librdf_parser_libwww_parse_from_uri(void *context, librdf_uri *uri) {
 
 static librdf_statement*
 librdf_parser_libwww_get_next_statement(librdf_parser_libwww_stream_context *context) {
-  return NULL;
+
+  if(!context->request_done) {
+    context->statements=librdf_new_list();
+    if(!context->statements)
+      return NULL;
+    HTEventList_loop(context->request);
+    context->request_done=1;
+  }
+  
+  context->next=librdf_list_pop(context->statements);
+
+  if(!context->next)
+    context->end_of_stream=1;
+
+  return context->next;
 }
 
 
@@ -286,6 +381,7 @@ librdf_parser_libwww_serialise_end_of_stream(void* context)
 
   return (scontext->next == NULL);
 }
+
 
 static librdf_statement*
 librdf_parser_libwww_serialise_next_statement(void* context)
@@ -323,6 +419,12 @@ librdf_parser_libwww_serialise_finished(void* context)
 
     if(scontext->parser)
       HTRDF_delete(scontext->parser);
+
+    if(scontext->statements)
+      librdf_free_list(scontext->statements);
+
+    /* Terminate libwww */
+    HTProfile_delete();
 
     LIBRDF_FREE(librdf_parser_libwww_context, scontext);
   }
