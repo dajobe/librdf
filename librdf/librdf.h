841fd29bea52e8a4efdbe0b6649f8ca69afc9f50
Dave Beckett <dave@dajobe.org>
2000-08-18 09:32:49 +0000

    redland.h -> librdf.h

---
+++ librdf/librdf.h
@@ -0,0 +1,194 @@
+/* -*- Mode: c; c-basic-offset: 2 -*-
+ *
+ * redland.h - Redland RDF Application Framework main header
+ *
+ * $Id$
+ *
+ * Copyright (C) 2000 David Beckett - http://purl.org/net/dajobe/
+ * Institute for Learning and Research Technology, University of Bristol.
+ *
+ *    This package is Free Software available under either of two licenses
+ *    (see FAQS.html to see why):
+ * 
+ * 1. The GNU Lesser General Public License (LGPL)
+ * 
+ *    See http://www.gnu.org/copyleft/lesser.html or COPYING.LIB for the
+ *    full license text.
+ *      _________________________________________________________________
+ * 
+ *      Copyright (C) 2000 David Beckett, Institute for Learning and
+ *      Research Technology, University of Bristol. All Rights Reserved.
+ * 
+ *      This library is free software; you can redistribute it and/or
+ *      modify it under the terms of the GNU Lesser General Public License
+ *      as published by the Free Software Foundation; either version 2 of
+ *      the License, or (at your option) any later version.
+ * 
+ *      This library is distributed in the hope that it will be useful, but
+ *      WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *      Lesser General Public License for more details.
+ * 
+ *      You should have received a copy of the GNU Lesser General Public
+ *      License along with this library; if not, write to the Free Software
+ *      Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ *      USA
+ *      _________________________________________________________________
+ * 
+ *    NOTE - under Term 3 of the LGPL, you may choose to license the entire
+ *    library under the GPL. See COPYING for the full license text.
+ * 
+ * 2. The Mozilla Public License
+ * 
+ *    See http://www.mozilla.org/MPL/MPL-1.1.html or MPL.html for the full
+ *    license text.
+ * 
+ *    Under MPL section 13. I declare that all of the Covered Code is
+ *    Multiple Licensed:
+ *      _________________________________________________________________
+ * 
+ *      The contents of this file are subject to the Mozilla Public License
+ *      version 1.1 (the "License"); you may not use this file except in
+ *      compliance with the License. You may obtain a copy of the License
+ *      at http://www.mozilla.org/MPL/
+ * 
+ *      Software distributed under the License is distributed on an "AS IS"
+ *      basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ *      the License for the specific language governing rights and
+ *      limitations under the License.
+ * 
+ *      The Initial Developer of the Original Code is David Beckett.
+ *      Portions created by David Beckett are Copyright (C) 2000 David
+ *      Beckett, Institute for Learning and Research Technology, University
+ *      of Bristol. All Rights Reserved.
+ * 
+ *      Alternatively, the contents of this file may be used under the
+ *      terms of the GNU Lesser General Public License, in which case the
+ *      provisions of the LGPL License are applicable instead of those
+ *      above. If you wish to allow use of your version of this file only
+ *      under the terms of the LGPL License and not to allow others to use
+ *      your version of this file under the MPL, indicate your decision by
+ *      deleting the provisions above and replace them with the notice and
+ *      other provisions required by the LGPL License. If you do not delete
+ *      the provisions above, a recipient may use your version of this file
+ *      under either the MPL or the LGPL License.
+ */
+
+
+#ifndef LIBRDF_H
+#define LIBRDF_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/* forward references to private structures */
+typedef struct librdf_hash_s librdf_hash;
+typedef struct librdf_hash_cursor_s librdf_hash_cursor;
+typedef struct librdf_digest_s librdf_digest;
+typedef struct librdf_uri_s librdf_uri;
+typedef struct librdf_list_s librdf_list;
+typedef struct librdf_iterator_s librdf_iterator;
+typedef struct librdf_node_s librdf_node;
+typedef struct librdf_statement_s librdf_statement;
+typedef struct librdf_model_s librdf_model;
+typedef struct librdf_storage_s librdf_storage;
+typedef struct librdf_stream_s librdf_stream;
+typedef struct librdf_context_s librdf_context;
+typedef struct librdf_parser_s librdf_parser;
+
+
+/* error handling */
+#ifdef LIBRDF_DEBUG
+/* DEBUGGING TURNED ON */
+
+/* Debugging messages */
+#define LIBRDF_DEBUG1(function, msg) do {fprintf(stderr, "%s:%d:%s: " msg, __FILE__, __LINE__, #function); } while(0)
+#define LIBRDF_DEBUG2(function, msg, arg1) do {fprintf(stderr, "%s:%d:%s: " msg, __FILE__, __LINE__, #function, arg1);} while(0)
+#define LIBRDF_DEBUG3(function, msg, arg1, arg2) do {fprintf(stderr, "%s:%d:%s: " msg, __FILE__, __LINE__, #function, arg1, arg2);} while(0)
+#define LIBRDF_DEBUG4(function, msg, arg1, arg2, arg3) do {fprintf(stderr, "%s:%d:%s: " msg, __FILE__, __LINE__, #function, arg1, arg2, arg3);} while(0)
+
+#else
+/* DEBUGGING TURNED OFF */
+
+/* No debugging messages */
+#define LIBRDF_DEBUG1(function, msg)
+#define LIBRDF_DEBUG2(function, msg, arg1)
+#define LIBRDF_DEBUG3(function, msg, arg1, arg2)
+#define LIBRDF_DEBUG4(function, msg, arg1, arg2, arg3)
+
+#endif
+
+
+/* for the memory allocation functions */
+#if defined(HAVE_DMALLOC_H) && defined(LIBRDF_MEMORY_DEBUG_DMALLOC)
+#include <dmalloc.h>
+#undef HAVE_STDLIB_H
+#endif
+
+
+#ifdef LIBRDF_MEMORY_DEBUG
+/* DEBUGGING MEMORY ALLOCATIONS */
+
+void* librdf_malloc(char *file, int line, char *type, size_t size);
+void* librdf_calloc(char *file, int line, char *type, size_t nmemb, size_t size);
+void librdf_free(char *file, int line, char *type, void *ptr);
+
+#define LIBRDF_MALLOC(type, size) librdf_malloc(__FILE__, __LINE__, #type, size)
+#define LIBRDF_CALLOC(type, size, count) librdf_calloc(__FILE__, __LINE__, #type, size, count)
+#define LIBRDF_FREE(type, ptr) librdf_free(__FILE__, __LINE__, #type, ptr)
+
+void librdf_memory_report(FILE *fh);
+
+#else
+/* NOT DEBUGGING MEMORY ALLOCATIONS */
+
+#ifdef HAVE_STDLIB_H
+#include <stdlib.h>
+#undef HAVE_STDLIB_H
+#endif
+
+#define LIBRDF_MALLOC(type, size) malloc(size)
+#define LIBRDF_CALLOC(type, size, count) calloc(size, count)
+#define LIBRDF_FREE(type, ptr)   free(ptr)
+
+#endif
+
+
+/* Fatal errors - always happen */
+#define LIBRDF_FATAL1(function, msg) do {fprintf(stderr, "%s:%d:%s: fatal error: " msg, __FILE__, __LINE__ , #function); abort();} while(0)
+#define LIBRDF_FATAL2(function, msg,arg) do {fprintf(stderr, "%s:%d:%s: fatal error: " msg, __FILE__, __LINE__ , #function, arg); abort();} while(0)
+
+
+/* public interfaces */
+
+/* from rdf_init.c */
+void librdf_init_world(char *digest_factory_name, librdf_hash* uris_hash);
+void librdf_destroy_world(void);
+
+/* from rdf_heuristics.c */
+int librdf_heuristic_object_is_literal(char *object);
+
+
+/* the rest should be automatically pulled from rdf_*.h headers */
+
+#include <rdf_list.h>
+#include <rdf_iterator.h>
+#include <rdf_uri.h>
+#include <rdf_hash.h>
+#include <rdf_digest.h>
+#include <rdf_node.h>
+#include <rdf_statement.h>
+#include <rdf_model.h>
+#include <rdf_storage.h>
+#include <rdf_stream.h>
+#include <rdf_context.h>
+#include <rdf_parser.h>
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
