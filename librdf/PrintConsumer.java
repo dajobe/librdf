485af1c3aac07003cb210a6f9ca645b272e3d5a8
Dave Beckett <dave@dajobe.org>
2000-08-28 23:19:54 +0000

    SiRPAC RDF consumer that prints out triple as they are added

---
+++ librdf/PrintConsumer.java
@@ -0,0 +1,131 @@
+/* -*- Mode: java -*-
+ *
+ * PrintConsumer.java - SiRPAC RDF consumer that prints out triple as they are added
+ *
+ * $Id$
+ *
+ * Copyright (C) 2000 David Beckett - http://purl.org/net/dajobe/
+ * Institute for Learning and Research Technology, University of Bristol.
+ *
+ *    This package is Free Software available under either of two licenses
+ *    (see FAQS.html to see why):
+ * 
+ * 1. The GNU Lesser General Public License (LGPL)
+ * 
+ *    See http://www.gnu.org/copyleft/lesser.html or COPYING.LIB for the
+ *    full license text.
+ *      _________________________________________________________________
+ * 
+ *      Copyright (C) 2000 David Beckett, Institute for Learning and
+ *      Research Technology, University of Bristol. All Rights Reserved.
+ * 
+ *      This library is free software; you can redistribute it and/or
+ *      modify it under the terms of the GNU Lesser General Public License
+ *      as published by the Free Software Foundation; either version 2 of
+ *      the License, or (at your option) any later version.
+ * 
+ *      This library is distributed in the hope that it will be useful, but
+ *      WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *      Lesser General Public License for more details.
+ * 
+ *      You should have received a copy of the GNU Lesser General Public
+ *      License along with this library; if not, write to the Free Software
+ *      Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ *      USA
+ *      _________________________________________________________________
+ * 
+ *    NOTE - under Term 3 of the LGPL, you may choose to license the entire
+ *    library under the GPL. See COPYING for the full license text.
+ * 
+ * 2. The Mozilla Public License
+ * 
+ *    See http://www.mozilla.org/MPL/MPL-1.1.html or MPL.html for the full
+ *    license text.
+ * 
+ *    Under MPL section 13. I declare that all of the Covered Code is
+ *    Multiple Licensed:
+ *      _________________________________________________________________
+ * 
+ *      The contents of this file are subject to the Mozilla Public License
+ *      version 1.1 (the "License"); you may not use this file except in
+ *      compliance with the License. You may obtain a copy of the License
+ *      at http://www.mozilla.org/MPL/
+ * 
+ *      Software distributed under the License is distributed on an "AS IS"
+ *      basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ *      the License for the specific language governing rights and
+ *      limitations under the License.
+ * 
+ *      The Initial Developer of the Original Code is David Beckett.
+ *      Portions created by David Beckett are Copyright (C) 2000 David
+ *      Beckett, Institute for Learning and Research Technology, University
+ *      of Bristol. All Rights Reserved.
+ * 
+ *      Alternatively, the contents of this file may be used under the
+ *      terms of the GNU Lesser General Public License, in which case the
+ *      provisions of the LGPL License are applicable instead of those
+ *      above. If you wish to allow use of your version of this file only
+ *      under the terms of the LGPL License and not to allow others to use
+ *      your version of this file under the MPL, indicate your decision by
+ *      deleting the provisions above and replace them with the notice and
+ *      other provisions required by the LGPL License. If you do not delete
+ *      the provisions above, a recipient may use your version of this file
+ *      under either the MPL or the LGPL License.
+ */
+
+
+import org.w3c.rdf.model.*;
+import org.w3c.rdf.syntax.*;
+
+import java.io.*;
+
+
+/**
+ * Parse the RDF/XML and print it as this is done
+ */
+
+public class PrintConsumer implements RDFConsumer {
+  Model model;
+  NodeFactory nodeFactory;
+  OutputStream output;
+
+  /**
+   * @param an RDF model to fill with triples. This frequently will be an empty model.
+   */
+  public PrintConsumer(Model model, OutputStream output) throws ModelException {
+    this.model = model;
+    this.nodeFactory = model.getNodeFactory();
+    this.output = output;
+  }
+
+  /**
+   * start is called when parsing of data is started
+   */
+  public void startModel () {}
+  
+  /**
+   * end is called when parsing of data is ended
+   */
+  public void endModel () {}
+
+
+  public NodeFactory getNodeFactory() {
+    return nodeFactory;
+  }
+  
+  /**
+   * assert is called every time a new statement within
+   * RDF data model is added
+   */
+  public void addStatement (Statement s) {
+    System.out.println("Statement " + s);
+    // immediately remove statement
+    try {
+      model.remove(s);
+    } catch (Exception e) {
+      System.err.println("Error in removing statement - " + e);
+    }
+  }
+
+}
