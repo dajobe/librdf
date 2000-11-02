441a72d581797470e7c77b7b34e824526d8be590
Dave Beckett <dave@dajobe.org>
2000-11-02 22:11:02 +0000

    Updated for changed parser API: Constructor takes optional MIME type, URI of syntax Added error/warning handling methods and callbacks Added set/get parser features Modifications for 2 SiRPACs

--- librdf/rdf_parser.c
+++ librdf/rdf_parser.c
@@ -78,6 +78,9 @@
 #include <rdf_config.h>
 
 #include <stdio.h>
+#ifdef HAVE_STDARG_H
+#include <stdarg.h>
+#endif
 
 #define LIBRDF_INTERNAL 1
 #include <librdf.h>
@@ -93,14 +96,26 @@ static librdf_parser_factory *parsers=NULL;
 
 /* helper functions */
 static void
+librdf_free_parser_factory(librdf_parser_factory *factory) 
+{
+  if(factory->name)
+    LIBRDF_FREE(librdf_parser_factory, factory->name);
+  if(factory->mime_type)
+    LIBRDF_FREE(librdf_parser_factory, factory->mime_type);
+  if(factory->type_uri)
+    librdf_free_uri(factory->type_uri);
+  LIBRDF_FREE(librdf_parser_factory, factory);
+}
+
+
+static void
 librdf_delete_parser_factories(void)
 {
   librdf_parser_factory *factory, *next;
   
   for(factory=parsers; factory; factory=next) {
     next=factory->next;
-    LIBRDF_FREE(librdf_parser_factory, factory->name);
-    LIBRDF_FREE(librdf_parser_factory, factory);
+    librdf_free_parser_factory(factory);
   }
 }
 
@@ -108,14 +123,17 @@ librdf_delete_parser_factories(void)
 /**
  * librdf_parser_register_factory - Register a parser factory
  * @name: the name of the parser
+ * @mime_type: MIME type of the syntax (optional)
+ * @uri_string: URI of the syntax (optional)
  * @factory: function to be called to register the factor parameters
  * 
  **/
 void
-librdf_parser_register_factory(const char *name,
+librdf_parser_register_factory(const char *name, const char *mime_type,
+                               const char *uri_string,
 			       void (*factory) (librdf_parser_factory*))
 {
-  librdf_parser_factory *d, *parser;
+  librdf_parser_factory *parser_factory;
   char *name_copy;
 
 #if defined(LIBRDF_DEBUG) && LIBRDF_DEBUG > 1
@@ -123,41 +141,57 @@ librdf_parser_register_factory(const char *name,
 		"Received registration for parser %s\n", name);
 #endif
   
-  parser=(librdf_parser_factory*)LIBRDF_CALLOC(librdf_parser_factory, 1,
+  parser_factory=(librdf_parser_factory*)LIBRDF_CALLOC(librdf_parser_factory, 1,
 					       sizeof(librdf_parser_factory));
-  if(!parser)
+  if(!parser_factory)
     LIBRDF_FATAL1(librdf_parser_register_factory,
 		  "Out of memory\n");
   
   name_copy=(char*)LIBRDF_CALLOC(cstring, 1, strlen(name)+1);
   if(!name_copy) {
-    LIBRDF_FREE(librdf_parser, parser);
+    librdf_free_parser_factory(parser_factory);
     LIBRDF_FATAL1(librdf_parser_register_factory, "Out of memory\n");
   }
   strcpy(name_copy, name);
-  parser->name=name_copy;
-        
-  for(d = parsers; d; d = d->next ) {
-    if(!strcmp(d->name, name_copy)) {
-      LIBRDF_FATAL2(librdf_parser_register_factory,
-		    "parser %s already registered\n", d->name);
+  parser_factory->name=name_copy;
+
+  /* register mime type if any */
+  if(mime_type) {
+    char *mime_type_copy;
+    mime_type_copy=(char*)LIBRDF_CALLOC(cstring, 1, strlen(mime_type)+1);
+    if(!mime_type_copy) {
+      librdf_free_parser_factory(parser_factory);
+      LIBRDF_FATAL1(librdf_parser_register_factory, "Out of memory\n");
     }
+    strcpy(mime_type_copy, mime_type);
+    parser_factory->mime_type=mime_type_copy;
   }
 
+  /* register URI if any */
+  if(uri_string) {
+    librdf_uri *uri;
+
+    uri=librdf_new_uri(uri_string);
+    if(!uri) {
+      librdf_free_parser_factory(parser_factory);
+      LIBRDF_FATAL1(librdf_parser_register_factory, "Out of memory\n");
+    }
+    parser_factory->type_uri=uri;
+  }
   
         
   /* Call the parser registration function on the new object */
-  (*factory)(parser);
+  (*factory)(parser_factory);
 
 
 #if defined(LIBRDF_DEBUG) && LIBRDF_DEBUG > 1
   LIBRDF_DEBUG3(librdf_parser_register_factory,
 		"%s has context size %d\n", name,
-		parser->context_length);
+		parser_factory->context_length);
 #endif
   
-  parser->next = parsers;
-  parsers = parser;
+  parser_factory->next = parsers;
+  parsers = parser_factory;
   
 }
 
@@ -165,11 +199,14 @@ librdf_parser_register_factory(const char *name,
 /**
  * librdf_get_parser_factory - Get a parser factory by name
  * @name: the name of the factory
+ * @mime_type: the MIME type of the syntax (NULL if not used)
+ * @type_uri: URI of syntax (NULL if not used)
  * 
  * Return value: the factory or NULL if not found
  **/
 librdf_parser_factory*
-librdf_get_parser_factory(const char *name) 
+librdf_get_parser_factory(const char *name, const char *mime_type,
+                          librdf_uri *type_uri) 
 {
   librdf_parser_factory *factory;
 
@@ -182,16 +219,26 @@ librdf_get_parser_factory(const char *name)
     }
   } else {
     for(factory=parsers; factory; factory=factory->next) {
-      if(!strcmp(factory->name, name)) {
-	break;
-      }
+      /* next if name does not match */
+      if(name && strcmp(factory->name, name))
+	continue;
+
+      /* MIME type may need to match */
+      if(mime_type && factory->mime_type &&
+         strcmp(factory->mime_type, mime_type))
+        continue;
+      
+      /* URI may need to match */
+      if(type_uri && factory->type_uri &&
+         librdf_uri_equals(factory->type_uri, type_uri))
+        continue;
+
+      /* found it */
+      break;
     }
-    /* else FACTORY with name parser_name not found */
-    if(!factory) {
-      LIBRDF_DEBUG2(librdf_get_parser_factory,
-		    "No parser with name %s found\n", name);
+    /* else FACTORY with given arguments not found */
+    if(!factory)
       return NULL;
-    }
   }
   
   return factory;
@@ -201,15 +248,18 @@ librdf_get_parser_factory(const char *name)
 /**
  * librdf_new_parser - Constructor - create a new librdf_parser object
  * @name: the parser factory name
+ * @mime_type: the MIME type of the syntax (NULL if not used)
+ * @type_uri: URI of syntax (NULL if not used)
  * 
  * Return value: new &librdf_parser object or NULL
  **/
 librdf_parser*
-librdf_new_parser(char *name)
+librdf_new_parser(const char *name, const char *mime_type,
+                  librdf_uri *type_uri)
 {
   librdf_parser_factory* factory;
 
-  factory=librdf_get_parser_factory(name);
+  factory=librdf_get_parser_factory(name, mime_type, type_uri);
   if(!factory)
     return NULL;
 
@@ -240,7 +290,8 @@ librdf_new_parser_from_factory(librdf_parser_factory *factory)
         
   d->factory=factory;
 
-  factory->init(d->context);
+  if(factory->init)
+    factory->init(d, d->context);
 
   return d;
 }
@@ -254,8 +305,11 @@ librdf_new_parser_from_factory(librdf_parser_factory *factory)
 void
 librdf_free_parser(librdf_parser *parser) 
 {
-  if(parser->context)
+  if(parser->context) {
+    if(parser->factory->terminate)
+      parser->factory->terminate(parser->context);
     LIBRDF_FREE(parser_context, parser->context);
+  }
   LIBRDF_FREE(librdf_parser, parser);
 }
 
@@ -278,15 +332,16 @@ librdf_parser_parse_as_stream(librdf_parser* parser, librdf_uri* uri,
   const char *filename;
   
   if(parser->factory->parse_uri_as_stream)
-    return parser->factory->parse_uri_as_stream(parser->context, uri, base_uri);
+    return parser->factory->parse_uri_as_stream(parser->context,
+                                                uri, base_uri);
 
   filename=librdf_uri_as_filename(uri);
   if(!filename) {
     LIBRDF_DEBUG2(librdf_parser_parse_as_stream, "%s parser can only handle file: URIs\n", parser->factory->name);
     return NULL;
   }
-  return parser->factory->parse_file_as_stream(parser->context, uri,
-                                               base_uri);
+  return parser->factory->parse_file_as_stream(parser->context,
+                                               uri, base_uri);
 }
 
 
@@ -306,16 +361,16 @@ librdf_parser_parse_into_model(librdf_parser* parser, librdf_uri* uri,
   const char *filename;
   
   if(parser->factory->parse_uri_into_model)
-    return parser->factory->parse_uri_into_model(parser->context, uri,
-                                                 base_uri, model);
+    return parser->factory->parse_uri_into_model(parser->context,
+                                                 uri, base_uri, model);
   
   filename=librdf_uri_as_filename(uri);
   if(!filename) {
     LIBRDF_DEBUG2(librdf_parser_parse_into_stream, "%s parser can only handle file: URIs\n", parser->factory->name);
     return 1;
   }
-  return parser->factory->parse_file_into_model(parser->context, uri,
-                                                base_uri, model);
+  return parser->factory->parse_file_into_model(parser->context,
+                                                uri, base_uri, model);
 }
 
 
@@ -352,6 +407,121 @@ librdf_finish_parser(void)
 }
 
 
+/*
+ * librdf_parser_error - Error from a parser - Internal
+ **/
+void
+librdf_parser_error(librdf_parser* parser, const char *message, ...)
+{
+  va_list arguments;
+
+  if(parser->error_fn) {
+    parser->error_fn(parser->error_user_data, message);
+    return;
+  }
+  
+  fprintf(stderr, "%s parser error - ", parser->factory->name);
+  va_start(arguments, message);
+  vfprintf(stderr, message, arguments);
+  va_end(arguments);
+}
+
+
+/*
+ * librdf_parser_warning - Warning from a parser - Internal
+ **/
+void
+librdf_parser_warning(librdf_parser* parser, const char *message, ...)
+{
+  va_list arguments;
+
+  if(parser->warning_fn) {
+    parser->warning_fn(parser->warning_user_data, message);
+    return;
+  }
+  
+  fprintf(stderr, "%s parser warning - ", parser->factory->name);
+  va_start(arguments, message);
+  vfprintf(stderr, message, arguments);
+  va_end(arguments);
+}
+
+
+/**
+ * librdf_parser_set_error - Set the parser error handling function
+ * @parser: the parser
+ * @user_data: user data to pass to function
+ * @error_fn: pointer to the function
+ * 
+ * The function will receive callbacks when the parser fails.
+ * 
+ **/
+void
+librdf_parser_set_error(librdf_parser* parser, void *user_data,
+                        void (*error_fn)(void *user_data, const char *msg, ...))
+{
+  parser->error_user_data=user_data;
+  parser->error_fn=error_fn;
+}
+
+
+/**
+ * librdf_parser_set_warning - Set the parser warning handling function
+ * @parser: the parser
+ * @user_data: user data to pass to function
+ * @warning_fn: pointer to the function
+ * 
+ * The function will receive callbacks when the parser gives a warning.
+ * 
+ **/
+void
+librdf_parser_set_warning(librdf_parser* parser, void *user_data,
+                          void (*warning_fn)(void *user_data, const char *msg, ...))
+{
+  parser->warning_user_data=user_data;
+  parser->warning_fn=warning_fn;
+}
+
+
+/**
+ * librdf_parser_get_feature - Get the value of a parser feature
+ * @parser: parser object
+ * @feature: URI of feature
+ * 
+ * Return value: the value of the feature or NULL if no such feature
+ * exists or the value is empty.
+ **/
+const char *
+librdf_parser_get_feature(librdf_parser* parser, librdf_uri *feature) 
+{
+  if(parser->factory->get_feature)
+    return parser->factory->get_feature(parser->context, feature);
+
+  return NULL;
+}
+
+/**
+ * librdf_parser_set_feature - Set the value of a parser feature
+ * @parser: parser object
+ * @feature: URI of feature
+ * @value: value to set
+ * 
+ * Return value: non 0 on failure (negative if no such feature)
+ **/
+  
+int
+librdf_parser_set_feature(librdf_parser* parser, librdf_uri *feature, 
+                          const char *value) 
+{
+  if(parser->factory->set_feature)
+    return parser->factory->set_feature(parser->context, feature, value);
+
+  return (-1);
+}
+
+
+
+
 #ifdef STANDALONE
 
 /* one more prototype */
@@ -362,7 +532,7 @@ int
 main(int argc, char *argv[]) 
 {
   librdf_parser* d;
-  char *test_parser_types[]={"sirpac", "libwww", NULL};
+  char *test_parser_types[]={"sirpac-stanford", "sirpac-w3c", "libwww", "repat", NULL};
   int i;
   char *type;
   char *program=argv[0];
@@ -373,7 +543,7 @@ main(int argc, char *argv[])
   
   for(i=0; (type=test_parser_types[i]); i++) {
     fprintf(stderr, "%s: Trying to create new %s parser\n", program, type);
-    d=librdf_new_parser(type);
+    d=librdf_new_parser(type, NULL, NULL);
     if(!d) {
       fprintf(stderr, "%s: Failed to create new parser type %s\n", program, type);
       continue;
