c3202d617f634b20ffec268e6ba683ca784e288f
Dave Beckett <dave@dajobe.org>
2000-11-02 22:09:31 +0000

    Changed parser API: Constructor takes optional MIME type, URI of syntax Added error/warning handling methods and callbacks Added set/get parser features

--- librdf/rdf_parser.h
+++ librdf/rdf_parser.h
@@ -88,13 +88,27 @@ extern "C" {
 struct librdf_parser_factory_s 
 {
   struct librdf_parser_factory_s* next;
-  char *   name;
+  /* factory name - required */
+  char *name;
+  /* handle this MIME type/ Internet Media Type - optional */
+  char *mime_type;
+  /* handles the syntax defined by this URI - optional */
+  librdf_uri *type_uri;
 
   /* the rest of this structure is populated by the
      parser-specific register function */
   size_t  context_length;
 
-  int (*init)(void *c);
+  /* create a new parser */
+  int (*init)(librdf_parser* parser, void *c);
+
+  /* destroy a parser */
+  void (*terminate)(void *c);
+
+  /* get/set features of parser (think of Java properties) */
+  const char * (*get_feature)(void *c, librdf_uri *feature);
+  int (*set_feature)(void *c, librdf_uri *feature, const char *value);
+  
   librdf_stream* (*parse_uri_as_stream)(void *c, librdf_uri *uri, librdf_uri* base_uri);
   int (*parse_uri_into_model)(void *c, librdf_uri *uri, librdf_uri* base_uri, librdf_model *model);
   librdf_stream* (*parse_file_as_stream)(void *c, librdf_uri *uri, librdf_uri *base_uri);
@@ -105,14 +119,20 @@ typedef struct librdf_parser_factory_s librdf_parser_factory;
 
 struct librdf_parser_s {
   void *context;
+
+  void *error_user_data;
+  void *warning_user_data;
+  void (*error_fn)(void *user_data, const char *msg, ...);
+  void (*warning_fn)(void *user_data, const char *msg, ...);
+
   librdf_parser_factory* factory;
 };
 
 
 /* factory static methods */
-void librdf_parser_register_factory(const char *name, void (*factory) (librdf_parser_factory*));
+void librdf_parser_register_factory(const char *name, const char *mime_type, const char *uri_string, void (*factory) (librdf_parser_factory*));
 
-librdf_parser_factory* librdf_get_parser_factory(const char *name);
+librdf_parser_factory* librdf_get_parser_factory(const char *name, const char *mime_type, librdf_uri *type_uri);
 
 
 /* module init */
@@ -122,7 +142,7 @@ void librdf_finish_parser(void);
                     
 
 /* constructor */
-librdf_parser* librdf_new_parser(char *name);
+librdf_parser* librdf_new_parser(const char *name, const char *mime_type, librdf_uri *type_uri);
 librdf_parser* librdf_new_parser_from_factory(librdf_parser_factory *factory);
 
 /* destructor */
@@ -132,7 +152,15 @@ void librdf_free_parser(librdf_parser *parser);
 /* methods */
 librdf_stream* librdf_parser_parse_as_stream(librdf_parser* parser, librdf_uri* uri, librdf_uri* base_uri);
 int librdf_parser_parse_into_model(librdf_parser* parser, librdf_uri* uri, librdf_uri* base_uri, librdf_model* model);
+void librdf_parser_set_error(librdf_parser* parser, void *user_data, void (*error_fn)(void *user_data, const char *msg, ...));
+void librdf_parser_set_warning(librdf_parser* parser, void *user_data, void (*warning_fn)(void *user_data, const char *msg, ...));
 
+const char *librdf_parser_get_feature(librdf_parser* parser, librdf_uri *feature);
+int librdf_parser_set_feature(librdf_parser* parser, librdf_uri *feature, const char *value);
+
+/* internal callbacks used by parsers invoking errors/warnings upwards to user */
+void librdf_parser_error(librdf_parser* parser, const char *message, ...);
+void librdf_parser_warning(librdf_parser* parser, const char *message, ...);
 
 /* in librdf_parser_sirpac.c */
 #ifdef HAVE_SIRPAC_RDF_PARSER
