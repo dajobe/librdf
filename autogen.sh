f8823766dab07aff38900c8b56ed86714c2aa5f4
Dave Beckett <dave@dajobe.org>
2003-02-20 17:49:54 +0000

    Update to use automake 1.6, autoconf 2.52 min

--- autogen.sh
+++ autogen.sh
@@ -28,63 +28,163 @@ if test "X$DRYRUN" != X; then
   DRYRUN=echo
 fi
 
-autoconf_vers=2.13
-automake_vers=1.4
-aclocal_vers=1.4
-swig_vers=1.3.14
+program=`basename $0`
+
+# automake 1.7 requires autoconf 2.54
+# automake 1.6 requires autoconf 2.52
+automake_min_vers=1.6
+aclocal_min_vers=$automake_min_vers
+autoconf_min_vers=2.52
+libtoolize_min_vers=1.4
+swig_min_vers=1.3.14
+
+
+automake_args=--add-missing
+autoconf_args=
+aclocal_args=
+
 
 program=`basename $0`
 
-if (autoconf --version) < /dev/null > /dev/null 2>&1 ; then
-    if (autoconf --version | awk 'NR==1 { if( $3 >= '$autoconf_vers') \
+# START Find autoconf and automake
+save_args=${1+"$*"}
+save_ifs="$IFS"
+IFS=":"
+set - $PATH
+IFS="$save_ifs"
+
+autoconf=autoconf
+autoconf_vers=0
+automake=automake
+automake_vers=0
+aclocal=aclocal
+aclocal_vers=0
+
+here=`pwd`
+while [ $# -ne 0 ] ; do
+  dir=$1
+  shift
+  if [ ! -d $dir ]; then
+    continue
+  fi
+  cd $dir
+
+  # This should really be a shell function called 3 times
+  progs=`ls autoconf* 2>/dev/null`
+  if [ "X$progs" != "X" ]; then
+    for prog in $progs; do
+      vers=`$prog --version | sed -ne '1s/^.* //p'`
+      if [ "X$vers" = "X" ]; then
+        continue
+      fi
+      if expr $vers '>' $autoconf_vers >/dev/null; then
+        autoconf=$prog
+        autoconf_vers=$vers
+      fi
+    done
+  fi
+  progs=`ls automake* 2>/dev/null`
+  if [ "X$progs" != "X" ]; then
+    for prog in $progs; do
+      vers=`$prog --version | sed -ne '1s/^.* //p'`
+      if [ "X$vers" = "X" ]; then
+        continue
+      fi
+      if expr $vers '>' $automake_vers >/dev/null; then
+        automake=$prog
+        automake_vers=$vers
+      fi
+    done
+  fi
+  progs=`ls aclocal* 2>/dev/null`
+  if [ "X$progs" != "X" ]; then
+    for prog in $progs; do
+      vers=`$prog --version | sed -ne '1s/^.* //p'`
+      if [ "X$vers" = "X" ]; then
+        continue
+      fi
+      if expr $vers '>' $aclocal_vers >/dev/null; then
+        aclocal=$prog
+        aclocal_vers=$vers
+      fi
+    done
+  fi
+done
+cd $here
+
+set - $save_args
+# END Find autoconf and automake
+
+
+echo "$program: Found autoconf $autoconf version $autoconf_vers" 1>&2
+echo "$program: Found automake $automake version $automake_vers" 1>&2
+echo "$program: Found aclocal $aclocal version $aclocal_vers" 1>&2
+
+
+if ($autoconf --version) < /dev/null > /dev/null 2>&1 ; then
+    if ($autoconf --version | awk 'NR==1 { if( $3 >= '$autoconf_min_vers') \
 			       exit 1; exit 0; }');
     then
-       echo "$program: ERROR: \`autoconf' is too old."
-       echo "           (version $autoconf_vers or newer is required)"
+       echo "$program: ERROR: \`$autoconf' is too old."
+       echo "           (version $autoconf_min_vers or newer is required)"
        DIE="yes"
     fi
 else
     echo
-    echo "$program: ERROR: You must have \`autoconf' installed to compile $PACKAGE."
-    echo "           (version $autoconf_vers or newer is required)"
+    echo "$program: ERROR: You must have \`$autoconf' installed to compile $PACKAGE."
+    echo "           (version $autoconf_min_vers or newer is required)"
     DIE="yes"
 fi
 
-
-if (automake --version) < /dev/null > /dev/null 2>&1 ; then
-  if (automake --version | awk 'NR==1 { if( $4 >= '$automake_vers') \
+if ($automake --version) < /dev/null > /dev/null 2>&1 ; then
+  if ($automake --version | awk 'NR==1 { if( $4 >= '$automake_min_vers') \
 			     exit 1; exit 0; }');
      then
-     echo "$program: ERROR: \`automake' is too old."
-     echo "           (version $automake_vers or newer is required)"
+     echo "$program: ERROR: \`$automake' is too old."
+     echo "           (version $automake_min_vers or newer is required)"
      DIE="yes"
   fi
-  if (aclocal --version) < /dev/null > /dev/null 2>&1; then
-    if (aclocal --version | awk 'NR==1 { if( $4 >= '$aclocal_vers' ) \
+  if ($aclocal --version) < /dev/null > /dev/null 2>&1; then
+    if ($aclocal --version | awk 'NR==1 { if( $4 >= '$aclocal_min_vers' ) \
 						exit 1; exit 0; }' );
     then
-      echo "$program: ERROR: \`aclocal' is too old."
-      echo "           (version $aclocal_vers or newer is required)"
+      echo "$program: ERROR: \`$aclocal' is too old."
+      echo "           (version $aclocal_min_vers or newer is required)"
       DIE="yes"
     fi
   else
     echo
-    echo "$program: ERROR: Missing \`aclocal'"
-    echo "           The version of automake installed doesn't appear recent enough."
+    echo "$program: ERROR: Missing \`$aclocal'"
+    echo "           The version of $automake installed doesn't appear recent enough."
     DIE="yes"
   fi
 else
     echo
-    echo "$program: ERROR: You must have \`automake' installed to compile $PACKAGE."
-    echo "           (version $automake_vers or newer is required)"
+    echo "$program: ERROR: You must have \`$automake' installed to compile $PACKAGE."
+    echo "           (version $automake_min_vers or newer is required)"
+    DIE="yes"
+fi
+
+if (libtoolize --version) < /dev/null > /dev/null 2>&1 ; then
+    if (libtoolize --version | awk 'NR==1 { if( $4 >= '$libtoolize_min_vers') \
+			       exit 1; exit 0; }');
+    then
+       echo "$program: ERROR: \`libtoolize' is too old."
+       echo "           (version $libtoolize_min_vers or newer is required)"
+       DIE="yes"
+    fi
+else
+    echo
+    echo "$program: ERROR: You must have \`libtoolize' installed to compile $PACKAGE."
+    echo "           (version $libtoolize_min_vers or newer is required)"
     DIE="yes"
 fi
 
 
 if (swig -help) </dev/null >/dev/null 2>&1; then 
   swig_version=`swig -version 2>&1 |sed -ne 's/^SWIG Version //p'`
-  swig_version_dec=`echo $SWIG_VERSION | awk -F. '{printf("%d\n", 10000*$1 + 100*$2 + $3)};'`
-  swig_min_version_dec=`echo $SWIG_MIN_VERSION | awk -F. '{printf("%d\n", 10000*$1 + 100*$2 + $3)};'`
+  swig_version_dec=`echo $swig_version | awk -F. '{printf("%d\n", 10000*$1 + 100*$2 + $3)};'`
+  swig_min_version_dec=`echo $swig_min_vers | awk -F. '{printf("%d\n", 10000*$1 + 100*$2 + $3)};'`
 
   if test $swig_version_dec -lt $swig_min_version_dec; then
     echo "$program: ERROR: \`swig' is too old."
@@ -104,10 +204,6 @@ if test "X$DIE" != X; then
 fi
 
 
-# Ensure that these are created by the versions on this system
-# (indirectly via automake)
-rm -f libtool ltmain.sh ltconfig
-
 if test -z "$*"; then
   echo "$program: WARNING: Running \`configure' with no arguments."
   echo "If you wish to pass any to it, please specify them on the"
@@ -116,32 +212,17 @@ fi
 
 am_opt=
 
-for coin in `find $SRCDIR -name configure.in -print`
-do 
-  dir=`dirname $coin`
-  if test -f $dir/NO-AUTO-GEN; then
-    echo $program: Skipping $dir -- flagged as no auto-gen
-  else
-    echo $program: Processing directory $dir
-    ( cd $dir
-      aclocalinclude="$ACLOCAL_FLAGS"
-      echo "$program: Running aclocal $aclocalinclude"
-      $DRYRUN aclocal $aclocalinclude
-      if grep "^AM_CONFIG_HEADER" configure.in >/dev/null; then
-	echo "$program: Running autoheader"
-	$DRYRUN autoheader
-      fi
-      echo "$program: Running automake $am_opt"
-      $DRYRUN automake --add-missing $am_opt
-      echo "$program: Running autoconf"
-      $DRYRUN autoconf
-    )
-  fi
-done
+# Ensure that these are created by the versions on this system
+# (indirectly via automake)
+rm -f ltconfig ltmain.sh libtool
+# Made by automake
+rm -f missing depcomp
+# automake junk
+rm -rf autom4te.cache
 
+echo "$program: Running libtoolize --copy --automake"
+$DRYRUN libtoolize --copy --automake
 
-# automake --add-missing or libtool may add older versions, so do this
-# afterwards.
 if test -d $CONFIG_DIR; then
   for file in config.guess config.sub; do
     cfile=$CONFIG_DIR/$file
@@ -151,6 +232,30 @@ if test -d $CONFIG_DIR; then
     fi
   done
 fi
+
+for coin in `find $SRCDIR -name configure.ac -print`
+do 
+  dir=`dirname $coin`
+  if test -f $dir/NO-AUTO-GEN; then
+    echo $program: Skipping $dir -- flagged as no auto-gen
+  else
+    echo $program: Processing directory $dir
+    ( cd $dir
+      aclocalinclude="$ACLOCAL_FLAGS"
+      echo "$program: Running aclocal $aclocalinclude"
+      $DRYRUN $aclocal $aclocal_args
+      if grep "^AM_CONFIG_HEADER" configure.ac >/dev/null; then
+	echo "$program: Running autoheader"
+	$DRYRUN autoheader
+      fi
+      echo "$program: Running automake $am_opt"
+      $DRYRUN $automake $automake_args $am_opt
+      echo "$program: Running autoconf"
+      $DRYRUN $autoconf $autoconf_args
+
+    )
+  fi
+done
 
 
 rm -f config.cache
