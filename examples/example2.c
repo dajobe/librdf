60687e6162d67d938dce58314b8484088bb599e0
Dave Beckett <dave@dajobe.org>
2003-08-26 21:17:34 +0000

    Updated to a slightly more useful example - parsing from string content and adding/checking/removing a statement.

--- examples/example2.c
+++ examples/example2.c
@@ -1,10 +1,10 @@
 /* -*- Mode: c; c-basic-offset: 2 -*-
  *
- * example2.c - Redland example code importing RDF as triples from a text file and storing them on disk as BDB hashes
+ * example2.c - Redland example code parsing RDF/XML from a string in memory and adding/checking/removing a statement
  *
  * $Id$
  *
- * Copyright (C) 2000-2001 David Beckett - http://purl.org/net/dajobe/
+ * Copyright (C) 2000-2003 David Beckett - http://purl.org/net/dajobe/
  * Institute for Learning and Research Technology - http://www.ilrt.org/
  * University of Bristol - http://www.bristol.ac.uk/
  * 
@@ -25,6 +25,7 @@
 #include <ctype.h>
 #include <string.h>
 #include <stdarg.h>
+#include <unistd.h>
 
 #include <redland.h>
 
@@ -32,8 +33,16 @@
 int main(int argc, char *argv[]);
 
 
-/* FIXME: Yeah?  What about it? */
-#define LINE_BUFFER_LEN 1024
+static const char *rdfxml_content="<?xml version=\"1.0\"?>\
+<rdf:RDF xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"\
+     xmlns:dc=\"http://purl.org/dc/elements/1.1/\">\
+  <rdf:Description rdf:about=\"http://purl.org/net/dajobe/\">\
+    <dc:title>Dave Beckett's Home Page</dc:title>\
+    <dc:creator>Dave Beckett</dc:creator>\
+    <dc:description>The generic home page of Dave Beckett.</dc:description>\
+  </rdf:Description> \
+</rdf:RDF>\
+";
 
 int
 main(int argc, char *argv[]) 
@@ -41,127 +50,102 @@ main(int argc, char *argv[])
   librdf_world* world;
   librdf_storage* storage;
   librdf_model* model;
+  librdf_parser* parser;
+  librdf_statement* statement;
+  librdf_uri* uri;
   char *program=argv[0];
-  librdf_uri *uri;
-  char buffer[LINE_BUFFER_LEN];
-  FILE *fh;
-  FILE *output=stdout;
+  int rc=0;
   
-  int line;
-
-  if(argc !=2) {
-    fprintf(stderr, "USAGE: %s: <RDF file>\n", program);
-    return(1);
-  }
-
-
   world=librdf_new_world();
   librdf_world_open(world);
 
-  uri=librdf_new_uri(world, argv[1]);
+
+  uri=librdf_new_uri(world, "http://example.librdf.org/");
+  
   if(!uri) {
     fprintf(stderr, "%s: Failed to create URI\n", program);
     return(1);
   }
 
-  storage=librdf_new_storage(world, "hashes", "test", "hash-type='bdb',dir='.'");
+  storage=librdf_new_storage(world, "memory", "test", NULL);
   if(!storage) {
     fprintf(stderr, "%s: Failed to create new storage\n", program);
-    return(1);
+    rc=1;
+    goto tidyworld;
   }
 
   model=librdf_new_model(world, storage, NULL);
   if(!model) {
     fprintf(stderr, "%s: Failed to create model\n", program);
-    return(1);
+    rc=1;
+    goto tidystorage;
+  }
+
+  parser=librdf_new_parser(world, "raptor", NULL, NULL);
+  if(!parser) {
+    fprintf(stderr, "%s: Failed to create new parser 'raptor'\n", program);
+    rc=1;
+    goto tidystorage;
+  }
+
+
+  if(librdf_parser_parse_string_into_model(parser, rdfxml_content, uri, model)) {
+    fprintf(stderr, "%s: Failed to parse RDF into model\n", program);
+    librdf_free_uri(uri);
+    rc=1;
+    goto tidymodel;
+  }
+
+  librdf_free_parser(parser); parser=NULL;
+  
+  librdf_free_uri(uri); uri=NULL;
+
+
+  statement=librdf_new_statement(world);
+  if(!statement) {
+    fprintf(stderr, "%s: Failed to parse RDF into model\n", program);
+    rc=1;
+    goto tidymodel;
   }
   
-
-  fh=fopen(argv[1], "r");
-  if(!fh) {
-    fprintf(stderr, "%s: Failed to open %s - \n", program, argv[1]);
-    perror(NULL);
-    return(1);
-  }
-
-  for(line=0; !feof(fh); line++) {
-    char *p, *s, *o, *e, *ptr;
-    librdf_statement* statement=NULL;
-
-
-    if(!fgets(buffer, LINE_BUFFER_LEN, fh))
-      continue;
-
-/*    fprintf(stderr, "%d:%s\n", line, buffer);*/
-
-    if(!(p=strstr(buffer, "triple(\"")))
-      continue;
-    
-    if(p != buffer)
-      continue;
-    
-    /* predicate found after >>triple("<< */
-    p+=8;
-
-    if(!(s=strstr(p, "\", \"")))
-      continue;
-    *s='\0';
-    s+=4;
-    
-    /* subject found after >>", "<< */
-
-    if(!(o=strstr(s, "\", ")))
-      continue;
-    *o='\0';
-    o+=4;
-
-    /* object found after >>", << */
-
-
-    if(!(e=strstr(o, "\")")))
-      continue;
-    /* zap end */
-    *e='\0';
-
-    /* trim trailing spaces at end of object */
-    for(ptr=e-1; isspace(*ptr); ptr--)
-      *ptr='\0';
-    
-
-    /* got all statement parts now */
-    statement=librdf_new_statement(world);
-    if(!statement)
-      break;
-    
-    librdf_statement_set_subject(statement, 
-                                 librdf_new_node_from_uri_string(world, s));
-
-    librdf_statement_set_predicate(statement,
-                                   librdf_new_node_from_uri_string(world, p));
-
-    if(librdf_heuristic_object_is_literal(o))
-      librdf_statement_set_object(statement,
-                                  librdf_new_node_from_literal(world, o, NULL, 0));
-    else
-      librdf_statement_set_object(statement, 
-                                  librdf_new_node_from_uri_string(world, o));
-
-
-    librdf_model_add_statement(model, statement);
-  }
-  fclose(fh);
-
-
-  fprintf(output, "%s: Resulting model is:\n", program);
-  librdf_model_print(model, output);
-
+  librdf_statement_set_subject(statement, 
+                               librdf_new_node_from_uri_string(world, "http://example.org/subject"));
   
+  librdf_statement_set_predicate(statement,
+                                   librdf_new_node_from_uri_string(world, "http://example.org/pred1"));
+  
+  librdf_statement_set_object(statement,
+                              librdf_new_node_from_literal(world, "object", NULL, 0));
+  
+  librdf_model_add_statement(model, statement);
+
+  fprintf(stdout, "%s: Resulting model is:\n", program);
+  librdf_model_print(model, stdout);
+
+  if(!librdf_model_contains_statement(model, statement)) {
+    fprintf(stdout, "%s: Model does not contain statement\n", program);
+    rc=1;
+    goto tidystatement;
+  } else
+    fprintf(stdout, "%s: Model contains the statement\n", program);
+
+
+  fprintf(stdout, "%s: Removing the statement\n", program);
+  librdf_model_remove_statement(model, statement);
+
+  fprintf(stdout, "%s: Resulting model is:\n", program);
+  librdf_model_print(model, stdout);
+
+ tidystatement:
+  librdf_free_statement(statement);
+
+ tidymodel:
   librdf_free_model(model);
 
+ tidystorage:
   librdf_free_storage(storage);
 
-  librdf_free_uri(uri);
-
+ tidyworld:
   librdf_free_world(world);
 
 #ifdef LIBRDF_MEMORY_DEBUG
@@ -169,5 +153,5 @@ main(int argc, char *argv[])
 #endif
 	
   /* keep gcc -Wall happy */
-  return(0);
+  return(rc);
 }
