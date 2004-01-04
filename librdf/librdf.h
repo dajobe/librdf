de97f41cfc12788f112dceb737ca637a5c2fd3b1
Dave Beckett <dave@dajobe.org>
2004-01-04 00:44:02 +0000

    Moved internal stuff to rdf_internal.h; include it.

--- librdf/librdf.h
+++ librdf/librdf.h
@@ -53,7 +53,7 @@ extern "C" {
 #endif
 
 
-/* forward references to private structures */
+/* Public typedefs (references to private structures) */
 typedef struct librdf_world_s librdf_world;
 typedef struct librdf_hash_s librdf_hash;
 typedef struct librdf_hash_cursor_s librdf_hash_cursor;
@@ -85,97 +85,22 @@ extern const unsigned int librdf_version_minor;
 extern const unsigned int librdf_version_release;
 extern const unsigned int librdf_version_decimal;
 
-/* error handling */
-#ifdef LIBRDF_DEBUG
-/* DEBUGGING TURNED ON */
-
-/* Debugging messages */
-#define LIBRDF_DEBUG1(function, msg) do {fprintf(stderr, "%s:%d:%s: " msg, __FILE__, __LINE__, #function); } while(0)
-#define LIBRDF_DEBUG2(function, msg, arg1) do {fprintf(stderr, "%s:%d:%s: " msg, __FILE__, __LINE__, #function, arg1);} while(0)
-#define LIBRDF_DEBUG3(function, msg, arg1, arg2) do {fprintf(stderr, "%s:%d:%s: " msg, __FILE__, __LINE__, #function, arg1, arg2);} while(0)
-#define LIBRDF_DEBUG4(function, msg, arg1, arg2, arg3) do {fprintf(stderr, "%s:%d:%s: " msg, __FILE__, __LINE__, #function, arg1, arg2, arg3);} while(0)
-
-#define LIBRDF_ERROR1(world, function, msg) do {fprintf(stderr, "%s:%d:%s: error: " msg, __FILE__, __LINE__ , #function); abort();} while(0)
-#define LIBRDF_ERROR2(world, function, msg,arg) do {fprintf(stderr, "%s:%d:%s: error: " msg, __FILE__, __LINE__ , #function, arg); abort();} while(0)
-#define LIBRDF_ERROR3(world, function, msg,arg1,arg2) do {fprintf(stderr, "%s:%d:%s: error: " msg, __FILE__, __LINE__ , #function, arg1, arg2); abort();} while(0)
-
-#if defined(HAVE_DMALLOC_H) && defined(LIBRDF_MEMORY_DEBUG_DMALLOC)
-void* librdf_system_malloc(size_t size);
-void librdf_system_free(void *ptr);
-#define SYSTEM_MALLOC(size)   librdf_system_malloc(size)
-#define SYSTEM_FREE(ptr)   librdf_system_free(ptr)
-#else
-#define SYSTEM_MALLOC(size)   malloc(size)
-#define SYSTEM_FREE(ptr)   free(ptr)
-#endif
-
-
-#else
-/* DEBUGGING TURNED OFF */
-
-/* No debugging messages */
-#define LIBRDF_DEBUG1(function, msg)
-#define LIBRDF_DEBUG2(function, msg, arg1)
-#define LIBRDF_DEBUG3(function, msg, arg1, arg2)
-#define LIBRDF_DEBUG4(function, msg, arg1, arg2, arg3)
-
-#define LIBRDF_ERROR1(world, function, msg) librdf_error(world, "%s:%d:%s: error: " msg, __FILE__, __LINE__ , #function)
-#define LIBRDF_ERROR2(world, function, msg, arg) librdf_error(world, "%s:%d:%s: error: " msg, __FILE__, __LINE__ , #function, arg)
-#define LIBRDF_ERROR3(world, function, msg, arg1, arg2) librdf_error(world, "%s:%d:%s: error: " msg, __FILE__, __LINE__ , #function, arg1, arg2)
-
-#define SYSTEM_MALLOC(size)   malloc(size)
-#define SYSTEM_FREE(ptr)   free(ptr)
-
-#endif
-
-
-/* for the memory allocation functions */
-#if defined(HAVE_DMALLOC_H) && defined(LIBRDF_MEMORY_DEBUG_DMALLOC)
-#ifdef HAVE_STDLIB_H
-#include <stdlib.h>
-#undef HAVE_STDLIB_H
-#endif
-#include <dmalloc.h>
-#endif
-
-
-#ifdef HAVE_STDLIB_H
-#include <stdlib.h>
-#undef HAVE_STDLIB_H
-#endif
-
-#define LIBRDF_MALLOC(type, size) malloc(size)
-#define LIBRDF_CALLOC(type, size, count) calloc(size, count)
-#define LIBRDF_FREE(type, ptr)   free(ptr)
-
-
-/* Fatal errors - always happen */
-#define LIBRDF_FATAL1(world, function, msg) do {fprintf(stderr, "%s:%d:%s: fatal error: " msg "\n", __FILE__, __LINE__ , #function); abort();} while(0)
-#define LIBRDF_FATAL2(world, function, msg, arg) do {fprintf(stderr, "%s:%d:%s: fatal error: " msg "\n", __FILE__, __LINE__ , #function, arg); abort();} while(0)
-
-
 /* Required for va_list in error handler function registrations
  * which are in the public API
  */
 #include <stdarg.h>
 
 
-/* these includes should be replaced with automatically pulled
- * definitions from rdf_*.h headers 
- */
-
 /* internal interfaces  */
 #ifdef LIBRDF_INTERNAL
-
-#include <rdf_list.h>
-#include <rdf_hash.h>
-#include <rdf_digest.h>
-#include <rdf_files.h>
-#include <rdf_heuristics.h>
-
+#include <rdf_internal.h>
 #endif
 
 /* public interfaces */
+
+/* FIXME: Should be replaced with automatically pulled
+ * definitions from the listed rdf_*.h header files.
+ */
 #include <rdf_init.h>
 #include <rdf_iterator.h>
 #include <rdf_uri.h>
