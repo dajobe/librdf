323f6b7a9021110e99db0a3b407a2f9447443ec6
Dave Beckett <dave@dajobe.org>
2004-01-25 18:04:35 +0000

    (librdf_storage_hashes_get_contexts_get_method): World in error arg.

--- librdf/rdf_storage_hashes.c
+++ librdf/rdf_storage_hashes.c
@@ -1673,7 +1673,7 @@ librdf_storage_hashes_get_contexts_get_method(void* iterator, int flags)
       break;
       
     default:
-      LIBRDF_ERROR2(icontext->world, "Unknown iterator method flag %d\n", flags);
+      LIBRDF_ERROR2(icontext->storage->world, "Unknown iterator method flag %d\n", flags);
       result=NULL;
       break;
   }
