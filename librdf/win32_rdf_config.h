7fd118dc7f71daae0d59d7234b15d4d723079624
Dave Beckett <dave@dajobe.org>
2005-01-13 23:29:03 +0000

    Do not define REDLAND_INTERNAL at all. LIBRDF_INTERNAL should be defined in the build configuration

--- librdf/win32_rdf_config.h
+++ librdf/win32_rdf_config.h
@@ -34,10 +34,6 @@ extern "C" {
 
 #define WIN32_LEAN_AND_MEAN 1
 
-/* Compiling inside Redland */
-#define REDLAND_INTERNAL 1
-#define LIBRDF_INTERNAL 1
-
 /* getopt is not in standard win32 C library - define if we have it */
 /* #define HAVE_GETOPT_H 1 */
 
