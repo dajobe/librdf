28e4e03b1416b448ec7a6c2cd4c3f048958d632a
Dave Beckett <dave@dajobe.org>
2006-10-08 06:40:06 +0000

    struct librdf_list_iterator_context_s looses the next field.

--- librdf/rdf_list_internal.h
+++ librdf/rdf_list_internal.h
@@ -53,7 +53,6 @@ struct librdf_list_iterator_context_s {
   librdf_iterator* iterator;
   librdf_list* list;
   librdf_list_node *current;
-  librdf_list_node *next;
   struct librdf_list_iterator_context_s* next_ic;
   struct librdf_list_iterator_context_s* prev_ic;
 };
