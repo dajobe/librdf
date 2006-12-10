0af4b7ba49e24b4a3ed90f176eed206201d09f54
Dave Beckett <dave@dajobe.org>
2006-12-10 05:50:15 +0000

    (librdf_storage_stream_to_node_iterator_get_method): Do not try to copy a NULL context node.

--- librdf/rdf_storage.c
+++ librdf/rdf_storage.c
@@ -866,7 +866,7 @@ librdf_storage_stream_to_node_iterator_get_method(void* iterator, int flags)
     case LIBRDF_ITERATOR_GET_METHOD_GET_CONTEXT:
       if(!context->context_node) {
         node=(librdf_node*)librdf_stream_get_context(context->stream);
-        context->context_node=librdf_new_node_from_node(node);
+        context->context_node=node ? librdf_new_node_from_node(node) : NULL;
       }
       node=context->context_node;
       break;
