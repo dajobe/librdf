f9ef9069c8f632d6dc4f417d82ff9e2728a7375e
Dave Beckett <dave@dajobe.org>
2008-07-01 06:13:49 +0000

    Make librdf_parser_parse_file_handle_as_stream arg be fh to match docs.

--- librdf/rdf_parser.h
+++ librdf/rdf_parser.h
@@ -63,7 +63,7 @@ librdf_stream* librdf_parser_parse_string_as_stream(librdf_parser* parser, const
 REDLAND_API
 int librdf_parser_parse_string_into_model(librdf_parser* parser, const unsigned char *string, librdf_uri* base_uri, librdf_model* model);
 REDLAND_API
-librdf_stream* librdf_parser_parse_file_handle_as_stream(librdf_parser* parser, FILE* handle, int close_fh, librdf_uri* base_uri);
+librdf_stream* librdf_parser_parse_file_handle_as_stream(librdf_parser* parser, FILE* fh, int close_fh, librdf_uri* base_uri);
 REDLAND_API
 int librdf_parser_parse_file_handle_into_model(librdf_parser* parser, FILE *fh, int close_fh, librdf_uri* base_uri, librdf_model* model);
 REDLAND_API REDLAND_DEPRECATED
