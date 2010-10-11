d968fac14f92c94afd8f9fd055bb1b49a54a7cec
Dave Beckett <dave@dajobe.org>
2010-10-10 18:58:13 -0700

    Bump version to 1.0.12

--- src/win32_rdf_config.h
+++ src/win32_rdf_config.h
@@ -273,7 +273,7 @@ extern "C" {
 /* #define TIME_WITH_SYS_TIME*/
 
 /* Version number of package */
-#define VERSION "1.0.11"
+#define VERSION "1.0.12"
 
 /* Use POSIX threads */
 #define WITH_THREADS
