2924a698aeb958c694b05f0f03f711eef1857d43
Dave Beckett <dave@dajobe.org>
2013-04-07 16:53:20 -0700

    memset fixes [gcc 4.8]

--- src/rdf_digest_md5.c
+++ src/rdf_digest_md5.c
@@ -210,7 +210,7 @@ static void MD5Final(struct MD5Context *ctx)
   MD5Transform(ctx->buf, (u32 *) ctx->in);
   byteReverse((unsigned char *) ctx->buf, 4);
   memcpy(ctx->digest, ctx->buf, 16);
-  memset(ctx, 0, sizeof(ctx));	/* In case it's sensitive */
+  memset(ctx, 0, sizeof(*ctx));	/* In case it's sensitive */
 }
 
 
