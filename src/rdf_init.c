fb0d2a7290287af4091ce07cbd4f92513b53e1f0
Dave Beckett <dave@dajobe.org>
2024-10-01 22:01:12 -0700

    Remove obsolete autoconf macros

--- src/rdf_init.c
+++ src/rdf_init.c
@@ -45,15 +45,11 @@
 #endif
 
 /* for gettimeofday */
-#if TIME_WITH_SYS_TIME
+#ifdef HAVE_SYS_TIME_H
 #include <sys/time.h>
-#include <time.h>
-#else
-#if HAVE_SYS_TIME_H
-#include <sys/time.h>
-#else
-#include <time.h>
 #endif
+#ifdef HAVE_TIME_H
+#include <time.h>
 #endif
 
 #include <redland.h>
